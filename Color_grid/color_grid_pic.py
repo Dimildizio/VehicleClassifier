5b4da966d3ec41fc6ee1ae9b8b194ed3e90090be
Dimildizio <42382713+Dimildizio@users.noreply.github.com>
2023-06-01 01:34:18 +0300

    Add grid xy lines

--- Color_grid/color_grid_pic.py
+++ Color_grid/color_grid_pic.py
@@ -2,17 +2,23 @@ import cv2
 import numpy as np
 
 # Initialize the image and colors
-img_name = 'grid_pic.png'
+img_name = 'raw_pic.png'
 GRID = 40
 
-image = cv2.imread(img_name)
-#Set different semi-transparent colors
+IMAGE = cv2.imread(img_name)
+# Set different semi-transparent colors
 COLORS = [(0, 0, 255, 128), (255, 0, 0, 128), (0, 255, 0, 128), (0, 255, 255, 128),
-          (255, 0, 255, 128), (75, 0, 130, 128), (255, 165, 0, 128),(0, 255, 165, 128)]
+          (255, 0, 255, 128), (75, 0, 130, 128), (255, 165, 0, 128), (0, 255, 165, 128)]
 COLOR_IND = 0
+black = (0, 0, 0)
+for x in range(0, IMAGE.shape[1], GRID):
+    cv2.line(IMAGE, (x, 0), (x, IMAGE.shape[0]), black, 1)
+for y in range(0, IMAGE.shape[0], GRID):
+    cv2.line(IMAGE, (0, y), (IMAGE.shape[1], y), black, 1)
+
 
 def draw_cube(event, x, y, flags, param):
-    global image, COLOR_IND
+    global IMAGE, COLOR_IND
     if event == cv2.EVENT_LBUTTONDOWN:
         # Get mouse cell position
         grid_x = x // GRID
@@ -25,9 +31,9 @@ def draw_cube(event, x, y, flags, param):
         end_y = start_y + GRID
 
         # Draw a cube
-        overlay = image.copy()
+        overlay = IMAGE.copy()
         cv2.rectangle(overlay, (start_x, start_y), (end_x, end_y), COLORS[COLOR_IND], -1, cv2.LINE_AA)
-        image = cv2.addWeighted(overlay, 0.5, image, 0.5, 0)
+        IMAGE = cv2.addWeighted(overlay, 0.5, IMAGE, 0.5, 0)
 
     # Scroll through colors
     elif event == cv2.EVENT_MOUSEWHEEL:
@@ -36,16 +42,17 @@ def draw_cube(event, x, y, flags, param):
         else:
             COLOR_IND = (COLOR_IND - 1) % len(COLORS)
 
+
 # Create a window and set the mouse callback func
 cv2.namedWindow('Image')
 cv2.setMouseCallback('Image', draw_cube)
 
 while True:
-    cv2.imshow('Image', image)
+    cv2.imshow('Image', IMAGE)
     # Check  events
     key = cv2.waitKey(1) & 0xFF
     # Exit on 'q'
     if key == ord('q'):
         break
-cv2.imwrite('modified_grid_pic.png', image)
+cv2.imwrite('modified_grid_pic.png', IMAGE)
 cv2.destroyAllWindows()
